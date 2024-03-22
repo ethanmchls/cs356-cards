f6a6c66787015312410f7da8e8152576f9df9d0d
ethanmchls <ethanmchls@gmail.com>
2024-03-22 12:14:00 -0600

    modal menu and purchase links

--- app/src/GameList.js
+++ app/src/GameList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const GameComponent = ({ game }) => {
+const GameDescription = ({ game }) => {
   return (
-    <div dangerouslySetInnerHTML={{ __html: game.description.replace(/\n/g, '<br>') }} />
+    <p dangerouslySetInnerHTML={{ __html: game.description.replace(/\n/g, '<br></br>') }} />
   );
 };
 
@@ -24,57 +24,83 @@ const GameRating = ({ game }) => {
   );
 };
 
+const GameModal = ({ game }) => {
+  return (
+    <dialog id={`${game.name}-modal`} className="modal">
+      <div className="modal-box bg-base-100 rounded-box !max-w-2xl">
+        <div className="flex flex-row text-sm">
+          <div className="flex flex-col">
+            <h1 className="text-2xl font-bold mb-4">{game.name}</h1>
+            <p className="text-lg"><b>Category: </b>{game.category}</p>
+            <p className="text-lg"><b>Play time: </b>{game.playTimeMin}-{game.playTimeMax} minutes</p>
+            <p className="text-lg"><b>Ages: </b>{game.ageMin}+ years old</p>
+            <p className="text-lg"><b>Group size:</b>{game.playerMin}-{game.playerMax} players</p>
+            <p className="text-lg"><b>Rating: </b>{game.rating}/10</p>
+          </div>
+          <figure className="aspect-square w-auto ml-auto">
+            <img src={`${game.image}`} alt="" className="hidden sm:block xs:w-0 sm:w-64" />
+          </figure>
+        </div>
+        <p className="py-4">
+          <b>Description:</b><br></br>
+        </p>
+        <GameDescription game={game} />
+        <br></br>
+        <a href={game.purchaseLink} target="_blank" rel="noreferrer" className="underline text-accent">Buy Now</a>
+        <div className="modal-action">
+          <form method="dialog">
+            <button className="btn btn-neutral-content hover:btn-secondary rounded-box border-2 border-secondary">Close</button>
+          </form>
+        </div>
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button></button>
+      </form>
+    </dialog>
+  );
+};
+
+const GameCard = ({ game }) => {
+  return (
+    <div
+      key={ game.name }
+      className="card w-auto h-full bg-base-200 shadow-md rounded-box hover:shadow-lg hover:scale-[102%]"
+      onClick={() =>
+        document.getElementById(`${game.name}-modal`).showModal()
+      }
+    >
+      <figure className="aspect-square"><img src={`${game.image}`} alt="" /></figure>
+      <div className="card-body">
+        <div className="card-title text-lg">{game.name}</div>
+        <div className="card-actions justify-center">
+          <div className="badge badge-outline border-secondary text-secondary">{game.ageMin}+</div> 
+          <div className="badge badge-outline border-secondary text-secondary">
+            {game.playerMin}-{game.playerMax}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-person ml-1"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+            </svg>
+          </div>
+          <GameRating game={game} />
+        </div>
+        <GameModal game={game} />
+      </div>
+    </div>
+  );
+};
+
 const GameList = ({ games }) => {
   return (
     <div className="w-full flex my-4">
       <div className="grid grid-flow-row text-neutral-600 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-between gap-4">
         {games.map((game) => (
-          <div
-            key={ game.name }
-            className="card w-auto h-full bg-base-200 shadow-md rounded-box hover:shadow-lg hover:scale-[102%]"
-            onClick={() =>
-              document.getElementById(`${game.name}-modal`).showModal()
-            }
-          >
-            <figure className="aspect-square"><img src={`${game.image}`} alt="" /></figure>
-            <div className="card-body">
-              <div className="card-title text-lg">{game.name}</div>
-              <div className="card-actions justify-center">
-                <div className="badge badge-outline border-secondary text-secondary">{game.ageMin}+</div> 
-                <div className="badge badge-outline border-secondary text-secondary">
-                  {game.playerMin}-{game.playerMax}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-person ml-1"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                  </svg>
-                </div>
-                <GameRating game={game} />
-              </div>
-              <dialog id={`${game.name}-modal`} className="modal">
-                <div className="modal-box bg-base-100 rounded-box">
-                  <h1 className="text-2xl font-bold">{game.name}</h1>
-                  <p className="py-4">
-                    <b>Game description:</b><br></br>
-                    <GameComponent game={game} />
-                  </p>
-                  <div className="modal-action">
-                    <form method="dialog">
-                      <button className="btn btn-neutral-content hover:btn-secondary rounded-box border-2 border-secondary">Close</button>
-                    </form>
-                  </div>
-                </div>
-                <form method="dialog" className="modal-backdrop">
-                  <button></button>
-                </form>
-              </dialog>
-            </div>
-          </div>
+          <GameCard key={game.name} game={game} />
         ))}
       </div>
     </div>
