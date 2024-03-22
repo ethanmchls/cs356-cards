b7d3b054894ff2952889d1c32675c17687df5098
ethanmchls <ethanmchls@gmail.com>
2024-03-22 11:15:29 -0600

    star ratings

--- app/src/GameList.js
+++ app/src/GameList.js
@@ -6,9 +6,26 @@ const GameComponent = ({ game }) => {
   );
 };
 
+const GameRating = ({ game }) => {
+  const filledStars = Math.round(game.rating);
+  return (
+    <div className="rating rating-md rating-half">
+      {Array.from({ length: 10 }).map((_, index) => (
+        <input
+          key={index}
+          type="radio"
+          name={`rating-${game.name}-${game.rating}`}
+          className={`bg-secondary mask mask-star-2 ${index % 2 === 0 ? 'mask-half-1' : 'mask-half-2'}`}
+          checked={index < filledStars && index + 1 >= filledStars}
+          readOnly
+        />
+      ))}
+    </div>
+  );
+};
+
 const GameList = ({ games }) => {
   return (
-    // <div className="mx-[6%] flex flex-wrap space-x-4 space-y-4">
     <div className="w-full flex my-4">
       <div className="grid grid-flow-row text-neutral-600 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-between gap-4">
         {games.map((game) => (
@@ -22,7 +39,7 @@ const GameList = ({ games }) => {
             <figure className="aspect-square"><img src={`${game.image}`} alt="" /></figure>
             <div className="card-body">
               <div className="card-title text-lg">{game.name}</div>
-              <div className="card-actions justify-end">
+              <div className="card-actions justify-center">
                 <div className="badge badge-outline border-secondary text-secondary">{game.ageMin}+</div> 
                 <div className="badge badge-outline border-secondary text-secondary">
                   {game.playerMin}-{game.playerMax}
@@ -37,7 +54,7 @@ const GameList = ({ games }) => {
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
                   </svg>
                 </div>
-                <div className="badge badge-secondary text-base-100">{game.rating}</div>
+                <GameRating game={game} />
               </div>
               <dialog id={`${game.name}-modal`} className="modal">
                 <div className="modal-box bg-base-100 rounded-box">
