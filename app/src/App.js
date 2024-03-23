32072eb27dfcdbc2d2326ade60547ac869e24485
ethanmchls <ethanmchls@gmail.com>
2024-03-23 13:47:04 -0600

    router and favicon

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,8 @@
 import { GameView } from "./GameView";
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Navigate to="/game-night" />} />
-          <Route path="/game-night" element={<GameView />} />
-        </Routes>
-      </div>
-    </Router>
+    <GameView />
   );
 };
 
