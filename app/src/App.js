193e3b3d88f757f0fe61e9de59dfa8ca39fc2f33
ethanmchls <ethanmchls@gmail.com>
2024-03-25 10:32:42 -0600

    added home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,15 @@
 import { GameView } from "./GameView";
-// import { Home } from "./Home";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { BrowserRouter as Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    // / should route to home, /explore should route to GameView
     // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/game-night" element={<GameView />} />
-    //   </Routes>
+      <Routes>
+        <Route path="/game-night" element={<Home />} />
+        <Route path="/game-night/explore" element={<GameView />} />
+      </Routes>
     // </Router>
-    <GameView />
   );
 };
 
