0d58ba2b760d815288b1f995e4b0d54b30adc324
ethanmchls <ethanmchls@gmail.com>
2024-03-25 10:04:50 -0600

    added home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,17 @@
 import { GameView } from "./GameView";
-import { Home } from "./Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { Home } from "./Home";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     // / should route to home, /explore should route to GameView
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game-night" element={<GameView />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/game-night" element={<GameView />} />
+    //   </Routes>
+    // </Router>
+    <GameView />
   );
 };
 
--- app/src/GameView.js
+++ app/src/GameView.js
@@ -34,7 +34,8 @@ export const GameView = () => {
     <div className="container mx-auto pt-4 w-screen h-screen flex flex-col">
       <div className="navbar bg-base-300 rounded-box sticky top-0 z-10 min-h-24">
         <div className="flex-1 px-2 lg:flex-none font-bold xl:text-6xl lg:text-5xl md:text-3xl sm:text-2xl">
-          <a href="/" className="hover:scale-105">Game Night</a>
+          {/* <a href="/" className="hover:scale-105">Game Night</a> */}
+          Game Night
           <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className="mx-3 bi bi-dice-5 xl:w-12 lg:w-10 md:w-8 w-4 hover:animate-spin" viewBox="0 0 16 16">
             <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3z"/>
             <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m-8 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m4-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
